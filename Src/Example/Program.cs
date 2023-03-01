4a443a93472c0e8ce07edd839e0a782bbcf85ace
Chris <filmee24@gmail.com>
2023-03-01 09:10:15 +0100

    Init

---
+++ Src/Example/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace  Example;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var db = new LiteDB.Sync.SyncedLiteDatabase(".\\test.db");
+        db.AfterSync += () =>
+        {
+            var users = db.GetCollection<User>().FindAll();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Username);
+            }
+        };
+
+        db.Start(Guid.Parse("B3559E46-3458-4A97-835E-EA8FE6FB00C6")); //id to identify your database
+
+
+        Console.WriteLine("Enter Username: ");
+        var username = Console.ReadLine();
+
+        db.BeginTrans();
+
+        db.GetCollection<User>().Insert(new User {Username = username});
+
+        db.Commit(); // commit triggers sync
+
+        var users = db.GetCollection<User>().FindAll();
+
+        foreach (var user in users)
+        {
+            Console.WriteLine(user.Username);
+        }
+    }
+}
---
+++ Src/Example/User.cs
@@ -0,0 +1,12 @@
+﻿using LiteDB;
+
+namespace Example;
+
+public class User
+{
+    [BsonId]
+    public ObjectId _id { get; set; }
+    
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
